984102563c9f264123888e954fa6063a5c98cde2
gabrielpassos2008 <gabrieldospa1109gmail.com>
2026-06-11 17:55:37 -0300

    finalizei a construção do cliente.

--- src/main/java/com/gabriel/van/model/Cliente.java
+++ src/main/java/com/gabriel/van/model/Cliente.java
@@ -18,6 +18,7 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
     private String senha;
