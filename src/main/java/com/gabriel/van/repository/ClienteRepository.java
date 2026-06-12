1a88ceeffe078b3d94c4ab5d3cac8ce2469cc53c
gabrielpassos2008 <gabrieldospa1109gmail.com>
2026-06-12 17:41:45 -0300

    ajuste no optional para ser usado o ispresent.

--- src/main/java/com/gabriel/van/repository/ClienteRepository.java
+++ src/main/java/com/gabriel/van/repository/ClienteRepository.java
@@ -1,12 +1,14 @@
 package com.gabriel.van.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gabriel.van.model.Cliente;
-import java.util.List;
+
 
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    List<Cliente> findByEmail(String email);
+    Optional<Cliente> findByEmail(String email);
 }
