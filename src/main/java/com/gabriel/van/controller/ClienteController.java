14c99f88eeb2bec28e3f1d46a2e0956921de3644
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-06-13 11:29:39 -0300

    - nova rota GET http criada, getpesquisarUsuario onde ela vai retorna o cliente ou uma mensagem de erro e codigo http

--- src/main/java/com/gabriel/van/controller/ClienteController.java
+++ src/main/java/com/gabriel/van/controller/ClienteController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gabriel.van.model.Cliente;
 import com.gabriel.van.service.ClienteService;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class ClienteController {
     private final ClienteService clienteService;
@@ -19,18 +20,31 @@ public class ClienteController {
     public ClienteController(ClienteService clienteService) {
         this.clienteService = clienteService;
     }
-    
+
     @PostMapping("/criarUsuario")
-    public ResponseEntity<?> criarUsuario(@RequestBody Cliente cliente) {
+    public ResponseEntity<?> postCriarUsuario(@RequestBody Cliente cliente) {
         if (clienteService.emailNaoExiste(cliente.getEmail())) {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body("Email já cadastrado!");
         }
-        Cliente criado = clienteService.cadastrarCliente(cliente);
+        Cliente cientecriado = clienteService.cadastrarCliente(cliente);
         return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(criado);
+                .status(HttpStatus.BAD_REQUEST)
+                .body(cientecriado);
     }
-    
+
+    @GetMapping("/pesquisarUsuario/{id}")
+    public ResponseEntity<?> getPesquisarUsario(@PathVariable Long id) {
+        Optional<Cliente> clientePesquisa = clienteService.pesquisarClientePorId(id);
+
+        if (clientePesquisa.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(clientePesquisa);
+        }
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("usuario do " + id + " nao existente no sistema");
+        // return clienteService.pesquisarClientePorId(id);
+    }
+
 }
