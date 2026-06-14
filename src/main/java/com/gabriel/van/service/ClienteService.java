60bedf2e2f363cbc4120341eb85daf7ec5d545c9
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-06-14 20:24:10 -0300

    - novo metedo para editar um cliente(com ajuste ainda)

--- src/main/java/com/gabriel/van/service/ClienteService.java
+++ src/main/java/com/gabriel/van/service/ClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.gabriel.van.model.Cliente;
 import com.gabriel.van.repository.ClienteRepository;
+
 @Service
 public class ClienteService {
 
@@ -15,21 +16,35 @@ public class ClienteService {
         this.repository = repository;
     }
 
-    public Cliente cadastrarCliente(Cliente cliente){
+    public Cliente cadastrarCliente(Cliente cliente) {
         return repository.save(cliente);
     };
 
-    public boolean emailNaoExiste(String email){
-        //retorna true se o email ja existe
+    public boolean emailNaoExiste(String email) {
+        // retorna true se o email ja existe
         return repository.findByEmail(email).isPresent();
     };
-    
-    public Optional<Cliente> pesquisarClientePorId(Long id){
-        return repository.findById(id);
-    }; 
 
-    public void deletarCLientePorId(Long id){
+    public Optional<Cliente> pesquisarClientePorId(Long id) {
+        return repository.findById(id);
+    };
+
+    public void deletarCLientePorId(Long id) {
         repository.deleteById(id);
-    }; 
-    
+    };
+
+    public Cliente atualizarCliente(Long id, Cliente dadosClienteNovo){
+        Cliente clienteNovo = repository.findById(id)
+            .orElseThrow();
+            // Atualiza os dados do cliente encontrado
+            // usando os dados recebidos na requisição.
+        clienteNovo.setEmail(dadosClienteNovo.getEmail());
+        clienteNovo.setNome(dadosClienteNovo.getNome());
+        clienteNovo.setSenha(dadosClienteNovo.getSenha());   
+            
+            // Como o objeto possui um ID que já existe no banco,
+            // o save() fará um UPDATE e não um INSERT. 
+        return repository.save(clienteNovo);
+    };
+
 }
