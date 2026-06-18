e92b5b245ac9fec00acc8ff183e508ffe34eca69
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-06-18 17:16:36 -0300

    - crie uma class DTO Respose que vai devolver os dados para o usuario.

---
+++ src/main/java/com/gabriel/van/dto/ClienteResposeDTO.java
@@ -0,0 +1,19 @@
+package com.gabriel.van.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+// o response serve para devolver dados ao usuario.
+// senha nao esta aqui pq é dados sensivel.
+public class ClienteResposeDTO {
+    private Long id;
+    private String nome;
+    private String email;
+}
