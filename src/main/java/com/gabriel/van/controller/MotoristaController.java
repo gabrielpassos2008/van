0df00a5013596e45588d10c3a1fc9c327249f7fc
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-06-30 17:42:53 -0300

    - criando a rota para cadastrar cliente.

--- src/main/java/com/gabriel/van/controller/MotoristaController.java
+++ src/main/java/com/gabriel/van/controller/MotoristaController.java
@@ -1,10 +1,19 @@
 package com.gabriel.van.controller;
 
+import com.gabriel.van.dto.ClienteDTO;
+import com.gabriel.van.dto.ClienteResponseDTO;
+import com.gabriel.van.dto.motorista.MotoristaCreateDTO;
+import com.gabriel.van.model.Cliente;
 import com.gabriel.van.service.ClienteService;
 import com.gabriel.van.service.MotoristaService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/motorista")
 public class MotoristaController {
 
     public final MotoristaService motoristaService;
@@ -13,5 +22,11 @@ public class MotoristaController {
     public MotoristaController(MotoristaService motoristaService, ClienteService clienteService){
         this.motoristaService = motoristaService;
         this.clienteService = clienteService;
+    }
+    @PostMapping("/cadastrarcliente")
+    public ResponseEntity<?> PostCadastrarCliente(@RequestBody ClienteDTO dto){
+        ClienteResponseDTO Cliente = clienteService.cadastrarCliente(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(Cliente);
+
     }
 }
